f5d732e568d5bc2fc2c23f60ffa0567c61514894
mackenzieporupsky <mack@porupsky.net>
2020-10-25 17:38:51 -0500

    finished form 3

--- forms/describeYou/describeYou.js
+++ forms/describeYou/describeYou.js
@@ -3,3 +3,12 @@
 
 
 
+
+rbnDescribe.onclick=function(){
+  let userChoice = $("input[name=rbnDescribe]:checked").prop("value")
+  lblResponse.value = `I would agree that you are a ${userChoice} person too!`
+}
+
+btnToNext.onclick=function(){
+  ChangeForm(favExercises)
+}
--- forms/favExercises/favExercises.js
+++ forms/favExercises/favExercises.js
@@ -2,11 +2,29 @@ var exercises = ["pullups","pushups","situps","jogging","plank","curls"]
 var coreExercises = ["situps","plank"]
 
 favExercises.onshow=function(){
+  selExercises.clear()
+  for (i=0; i <= exercises.length - 1; i++)
+    selExercises.addItem(exercises[i])
+  for (i=0; i <= coreExercises.length - 1; i++)
+    selExercises.addItem(coreExercises[i])
 
 }
 
-selExercises.onfocusout=function(){
 
+btnDone.onclick=function() {
+  if (selExercises.text.length == 2) { 
+    let message = "You chose " 
+    for (i = 0; i <= selExercises.text.length - 1;i++)
+      message = message + selExercises.text[i] + " and "
+    message = message.slice(0, -5);
+    message = `${message} - those are the two core exercises.`
+    lblMessage.value = message 
+  } else { 
+    lblMessage.value = "You did not pick the two core exercises."
+    }
 }
 
+btnForward.onclick=function() {
+  ChangeForm(mobileNav)
+}
 
