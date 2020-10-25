b473a493d33342d3fd4ca30343297a09000fe093
mackenzieporupsky <mack@porupsky.net>
2020-10-25 16:45:16 -0500

    Form 1
    form 1 complete

--- forms/dessertVoting/dessertVoting.js
+++ forms/dessertVoting/dessertVoting.js
@@ -3,20 +3,23 @@ var dessertList = ['Chocolate Cake','Cream Brulee','Cheesecake',
 
 dessertVoting.onshow=function(){
     drpDesserts.clear()   
-  /* add a loop here that adds all the desserts in the array 
-     to the dropdown.
-  */
+  for (i=0; i <= dessertList.length - 1; i++)
+    drpDesserts.addItem(dessertList[i])
 }
 
 
-drpDesserts.onclick=function(){
-   // this 'if' kicks user out if they  just clicked on control 
- // but not on one item in the list.
+drpDesserts.onclick=function(s){
     if (typeof(s) == "object")   
       return                    
     else {  // the user picked something
-        /* add code here to get item user chose and output 
-        it in a label, using a literal.
-        */
+      drpDesserts.value=s
+      lblOutput.value = `You picked ${s} - that is a great choice!`
     }
 }
+
+
+btnNext.onclick=function(){
+  ChangeForm(describeYou)
+}
+
+
