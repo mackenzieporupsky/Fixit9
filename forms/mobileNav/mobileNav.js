71b209610e278759ab52264572ea13bfa7b84f12
mackenzieporupsky <mack@porupsky.net>
2020-10-25 18:35:38 -0500

    form 4 complete

--- forms/mobileNav/mobileNav.js
+++ forms/mobileNav/mobileNav.js
@@ -1,8 +1,22 @@
 
 hmbMenu.onclick=function(s){
-  if (typeof(s) == "object") 
+  if (typeof(s) == "object") {
     return
-  /* add a switch here to take the user to the form
-     they chose. 
-  */
+  } else {
+  switch (s) { 
+  case "Login": 
+    ChangeForm(Login)
+    break
+  case "Dessert Voting": 
+    ChangeForm (dessertVoting)
+    break 
+  case "Describe Me":
+    ChangeForm (describeYou) 
+    break 
+  case "Favorite Exercises": 
+    ChangeForm (favExercises) 
+    break
+  }
+  }
 }
+
